49c1fdef70ec1c9f610198adf0c7fc58ea8495e9
al81205 <al81205@alunos.utad.pt>
2025-11-12 14:51:30 +0000

    a

---
+++ MarketplaceVeiculos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarketplaceVeiculos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
