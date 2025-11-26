4fc616eeef40202a42df216b6f08d1f7c038b873
DGomes-17 <al81454@alunos.utad.pt>
2025-11-26 18:22:28 +0000

    Projeto inicial

---
+++ DCarMarketplace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DCarMarketplace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
